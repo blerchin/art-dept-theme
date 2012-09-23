a6273172ecbd436de3ff91e8d0d2869cf4278c18
Ben Lerchin <ben@lerchin.com>
2012-09-23 11:25:46 -0700

    classy rollovers

--- js/behavior.js
+++ js/behavior.js
@@ -1,34 +1,38 @@
 $(document).ready( function(){
-	var elementToScroll = $('.slider.feed');
-	var feedItems = elementToScroll.children();
-	var fi = 0;
-	
-	var scrollInterval;
-	SetScrollIntervalAction();
-	
-	elementToScroll.mouseenter( function(){
-		clearInterval(scrollInterval);
-	});
-	
-	elementToScroll.mouseleave( function(){
-		SetScrollIntervalAction();	
-	});
-	function SetScrollIntervalAction(){
-		scrollInterval = setInterval(function() {
-				
-		  if( fi < feedItems.length -1 ) {
-			  fi++;
-		  } else {
-			  fi = 0;
-		  }
-		  elementToScroll.scrollTo(feedItems[fi], 500, {
-								  "axis":"y",
-								  "easing": "swing"
-								  });	
-		}, 10000);
-	}
-	
+	$('li.nav.list.item a').lettering();
+	setupVerticalSlider('.slider.feed');
+
 	
 });
 
+function setupVerticalSlider( container ) {
+	var elementToScroll = $(container);
+		var feedItems = elementToScroll.children();
+		var fi = 0;
+		
+		var scrollInterval;
+		SetScrollIntervalAction();
+		
+		elementToScroll.mouseenter( function(){
+			clearInterval(scrollInterval);
+		});
+		
+		elementToScroll.mouseleave( function(){
+			SetScrollIntervalAction();	
+		});
+		function SetScrollIntervalAction(){
+			scrollInterval = setInterval(function() {
+					
+			  if( fi < feedItems.length -1 ) {
+				  fi++;
+			  } else {
+				  fi = 0;
+			  }
+			  elementToScroll.scrollTo(feedItems[fi], 500, {
+									  "axis":"y",
+									  "easing": "swing"
+									  });	
+			}, 15000);
+		}
+}
 
