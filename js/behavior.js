9bf559b5c0b2449ec72c7b9333e1da73d2c7934e
Ben Lerchin <ben@lerchin.com>
2012-12-29 01:44:21 -0800

    code cleanup

--- js/behavior.js
+++ js/behavior.js
@@ -24,34 +24,24 @@ $(window).ready( function(){
 	var grid_el = gc.getElementsByTagName('svg')[0];
 	
 	var cp=document.createElementNS(svg,"clipPath");
-		cp.setAttribute("id","win");
+		cp.setAttribute("id","win-clip");
 		grid_el.appendChild(cp);
 		var boxes = R.path( window_panel.path);
 		cp.appendChild(boxes.node);
 
 
-	scale = {
-		bb: boxes.getBBox()
-	}
-	scale.w = (gc.w - scale.bb.width) / gc.w ;
-	scale.h = (gc.h - scale.bb.height) / gc.h;
-	scale.x = (gc.w - scale.bb.width) / 2;
-	scale.y = (gc.h - scale.bb.height) / 2
 	
+	scaleTo(boxes, gc.w, gc.h);	
 
-	boxes.transform("s"+(scale.w+1) +","+(scale.h+1)+","+0+","+0);
+	
 
 	for(i=0; i < window_panel.count; i++) {
 		var w = boxes.getBBox().width/3;
 		var h = boxes.getBBox().height/2;
-		console.log(w,h);
-		if( i < (window_panel.count/2) ) {
-			var x = i * w;
-			var y = 0;
-		} else if ( i <window_panel.count) {
-			var x = (i-window_panel.count/2) * w;
-			var y = h;
-		}
+
+		var x = i%(window_panel.count/2) * w;
+		var y = Math.floor(i/(window_panel.count/2)) * h;
+
 		
 		var g = document.createElementNS(svg,"g");
 			g.setAttribute('id','win'+i);
@@ -62,19 +52,15 @@ $(window).ready( function(){
 			g.appendChild(a);
 		
 		var src_img = grid_images[i];
-		console.log(src_img);
+
 		var ratio = src_img.height / src_img.width;
-		console.log(ratio);
-		var img = R.image(src_img.src,
-			x,
-			y,
-			w,
-			w*ratio
-		);
+
+		var img = R.image(src_img.src, x, y, w,	w*ratio	);
 			img.toFront();
+			
 			a.appendChild(img.node);
 			img.node.setAttribute("preserveAspectRatio", "xMinYMin");
- 			img.node.setAttribute("clip-path","url('#win')");
+ 			img.node.setAttribute("clip-path","url('#win-clip')");
 /*
 			var label = R.text(bb.x+(bb.width/2), bb.y+bb.height-10, img.text);
 			console.log(label);
@@ -99,8 +85,16 @@ $(window).ready( function(){
 	}
 });
 
+function scaleTo( object, width, height) {
+	bb = object.getBBox();
+	scale = {
+		w: (width - bb.width) / width,
+		h: (height - bb.height) / height
+	}
+	object.transform("s"+(scale.w+1) +","+(scale.h+1)+","+0+","+0);
+}
 
 var window_panel = {
-		path: "M263.09,168.128C178.443,168.095,93.795,168.061,9.148,168c0-52.333,0-104.667,0-157c84.542,6.535,169.184,11.029,253.942,13.468C263.09,72.354,263.09,120.242,263.09,168.128z M263.09,177.276C178.442,177.404,93.795,177.651,9.147,178c0.003,50,0,100,0,150c84.542-6.535,169.184-11.021,253.942-13.469C263.09,268.779,263.09,223.028,263.09,177.276z M527.616,24.692c-84.738,2.063-169.47,2.086-254.208,0.048c0,47.797,0.003,95.593,0,143.39c84.736,0.019,169.472,0.018,254.208-0.001C527.616,120.316,527.616,72.504,527.616,24.692zM527.616,177.265c-84.736-0.11-169.473-0.112-254.209-0.002c0.007,45.666,0.004,91.332,0.001,136.998c84.738-2.039,169.471-2.028,254.208,0.047C527.616,268.627,527.616,222.945,527.616,177.265z M790,11c-84.541,6.534-169.184,11.028-253.942,13.467c0,47.887,0,95.774,0,143.661c84.647-0.034,169.295-0.07,253.942-0.128C790,115.667,790,63.333,790,11z M790,178c-84.647-0.349-169.295-0.596-253.942-0.724c0,45.752,0,91.503,0,137.255c84.759,2.448,169.4,6.935,253.942,13.469C790,278,790,228,790,178z",
+		path: "M257.548,166.529c-88.35-0.037-176.698-0.072-265.048-0.138c0-56.13,0-112.261,0-168.392C80.739,5.009,169.082,9.829,257.548,12.446C257.548,63.806,257.548,115.168,257.548,166.529z M257.548,176.341c-88.35,0.137-176.699,0.401-265.048,0.776c0.003,53.628,0,107.256,0,160.883c88.239-7.009,176.582-11.819,265.048-14.446C257.548,274.482,257.548,225.412,257.548,176.341z M533.642,12.686c-88.443,2.212-176.881,2.237-265.325,0.05c0,51.265,0.003,102.529,0,153.794c88.442,0.02,176.884,0.019,265.325-0.001C533.642,115.249,533.642,63.967,533.642,12.686zM533.642,176.328c-88.441-0.118-176.885-0.121-265.326-0.002c0.007,48.979,0.004,97.959,0.001,146.937c88.443-2.186,176.882-2.175,265.325,0.052C533.642,274.319,533.642,225.323,533.642,176.328z M807.5-2C719.262,5.009,630.918,9.829,542.452,12.444c0,51.362,0,102.723,0,154.085c88.349-0.037,176.698-0.075,265.048-0.138C807.5,110.261,807.5,54.13,807.5-2z M807.5,177.117c-88.35-0.375-176.699-0.64-265.048-0.776c0,49.071,0,98.142,0,147.213c88.466,2.627,176.809,7.439,265.048,14.446C807.5,284.373,807.5,230.745,807.5,177.117z",
 		count: "6"
 	}
